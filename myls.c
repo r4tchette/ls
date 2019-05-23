ae83c72126d9209defb4ada30388672337c7df9d
r4tchette <r4tchette@gmail.com>
2019-05-23 22:40:55 +0900

    implement file permission and link number

--- myls.c
+++ myls.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
 #include <dirent.h>
+//#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+// "mode_t" is the type of stat.st_mode
+void printMode(mode_t mode){
+	printf( (S_ISDIR(mode)) ? "d" : "-" );
+	printf( (mode & S_IRUSR) ? "r" : "-" );
+	printf( (mode & S_IWUSR) ? "w" : "-" );
+	printf( (mode & S_IXUSR) ? "x" : "-" );
+	printf( (mode & S_IRGRP) ? "r" : "-" );
+	printf( (mode & S_IWGRP) ? "w" : "-" );
+	printf( (mode & S_IXGRP) ? "x" : "-" );
+	printf( (mode & S_IROTH) ? "r" : "-" );
+	printf( (mode & S_IWOTH) ? "w" : "-" );
+	printf( (mode & S_IXOTH) ? "x" : "-" );
+}
 
 void myls(char *path){
 	DIR *dp;
 	struct dirent *ep;
 	char *np;
+	struct stat info;
 
 	dp = opendir(path);
 	while(1){
 		ep = readdir(dp);
+		stat(ep->d_name, &info); //
+		
 		if(ep == NULL) break;
-		//np = ep->d_name;
-		printf("%s\n", ep->d_name);
+
+		//printf("[%10o] ", info.st_mode); //file permissions
+		printMode(info.st_mode);
+		printf(" %d ", info.st_nlink); //number of links
+		printf("[%10d] ", info.st_uid); //owner name
+		printf("[%10d] ", info.st_gid); //owner group
+		//file size
+		printf("[%10d] ", info.st_mtime); //time of last modification
+		printf("[%10s]\n", ep->d_name);//file/directory name
 	}
 	closedir(dp);
 }
